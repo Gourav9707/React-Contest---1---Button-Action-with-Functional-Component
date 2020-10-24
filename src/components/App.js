f7a38c711c8453cc32b13c8f9a0714aaf65494dd
Gourav9707 <67501980+Gourav9707@users.noreply.github.com>
2020-10-24 20:22:11 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,27 @@ import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
-  let [text, settext] = useState("");
-  function handleclick() {
-    settext(
-      "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
+  const [output, setOutput] = React.useState(false);
+
+  const handleClick = () => {
+    setOutput(true);
+    console.log(output);
+  };
+  function Para() {
+    return (
+      <p id="para">
+        Hello, I've learnt to use the full-stack evaluation tool. This makes me
+        so happy
+      </p>
     );
   }
   return (
     <div id="main">
-      <button id="click" onClick={handleclick()}>
-        <p id="para">click</p>
-        {text}
+      {/* // Do not alter the main div */}
+      <button id="click" onClick={handleClick}>
+        click
       </button>
+      {output ? <Para /> : ""}
     </div>
   );
 }
